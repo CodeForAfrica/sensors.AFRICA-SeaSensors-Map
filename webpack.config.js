7ae58f5eaeb15a06d5333034d1e72284e771dc9a
nerdlet <brndnyokabi@gmail.com>
2019-04-08 13:52:26 +0300

    Add webpack and config files

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+var webpack = require("webpack");
+
+const devMode = process.env.NODE_ENV !== "production";
+module.exports = {
+  mode: devMode ? "development" : "production",
+  entry: ["./src/js/main.js"],
+  output: {
+    path: path.resolve(__dirname, "public"),
+    publicPath: "/assets",
+    filename: "assets/js/bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      },
+      {
+        // Adds support to load images in your CSS rules. It looks
+        // for .png, .jpg, .jpeg and .gif
+        test: /\.(png|jpe?g|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              publicPath: "../images",
+              emitFile: false
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": process.env.NODE_ENV
+    })
+  ]
+};
