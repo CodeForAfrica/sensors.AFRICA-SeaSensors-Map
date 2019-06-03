a100b3fdb6002bced8888d5fdcc9f57538e12db9
karimkawambwa <4308339+karimkawambwa@users.noreply.github.com>
2019-06-03 15:22:26 +0300

    counter

--- sensors-africa-seasensors-map/src/App.tsx
+++ sensors-africa-seasensors-map/src/App.tsx
@@ -1,11 +1,13 @@
 import React, { Fragment } from 'react';
 import Mapbox from './components/Mapbox';
 import Filter from './components/Filter';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <Fragment>
       <Filter />
+      <Counter numberOfSensors={2} numberOfBlasts={42} />
       <Mapbox />
     </Fragment>
   );
