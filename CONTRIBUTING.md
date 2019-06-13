5ac6d151ada5085f9c0b866a7b0f28089687008b
nerdlet <brndnyokabi@gmail.com>
2019-06-13 17:53:51 +0300

    Revert "Updates"
    This reverts commit 0df42c53c294baeb984f6d218500e5ecaebc249e.

---
+++ CONTRIBUTING.md
@@ -0,0 +1,87 @@
+# Introduction
+
+All members of our community are expected to follow our
+[Code of Conduct](https://www.contributor-covenant.org/version/1/4/code-of-conduct). Please make
+sure you are welcoming and friendly in all of our spaces.
+
+Following these guidelines helps to communicate that you respect the time of
+the developers managing and developing this open source project. In return,
+they should reciprocate that respect in addressing your issue, assessing
+changes, and helping you finalize your pull requests.
+
+## Types of Contributions
+
+You can contribute in many ways. For example, you might:
+
+- Add documentation and "how-to" articles in the [README](README.md) or the wiki
+- Suggest Enhancements
+- Fix issues
+- Submit Bug reports
+
+### Writing Documentation
+
+Did you find a typo? Do you think that something should be clarified? Go ahead
+and suggest a documentation patch. sensors.AFRICA could always use more documentation,
+whether as part of the official docs, in docstrings, or even on the web in blog
+posts, wiki, articles, and such.
+
+### Fixing Issues
+
+Look through the GitHub issues for bugs. Anything tagged with "bug" is open to
+whoever wants to implement it.
+
+### Suggesting Enhancements
+
+Before creating enhancement suggestions, please check the issues list as you
+might find out that you don't need to create one. When you are creating an
+enhancement suggestion fill out the
+[issue template](https://github.com/CodeForAfricaLabs/ssensors.AFRICA-SeaSensors-Map/blob/master/.github/ISSUE_TEMPLATE.md)
+and [label](https://help.github.com/articles/creating-a-label/) the issue as a
+new feature.
+
+## Your first contribution
+
+Unsure where to begin contributing to sensors.AFRICA? You can start by looking through
+these `beginner` and `help-wanted` issues:
+
+- `Good first issue` - issues which should only require a few lines of code, and a
+  test or two. Good for newcomers
+- `Help wanted issues` - issues which should be a bit more involved than beginner
+  issues.
+
+Once your changes and tests are ready to submit for review:
+
+1. Test your changes
+
+   Run the tests if you have any and at the bare minimum, test your changes
+   manually.
+
+2. Rebase your changes
+
+   Update your local repository with the most recent code from the main sensors.AFRICA
+   repository, and rebase your branch on top of the latest develop branch.
+
+3. Submit a pull request
+
+   Push your local changes to your forked copy of the repository and submit a
+   pull request. In the pull request, choose a title which sums up the changes
+   that you have made, and in the body provide more details about what your
+   changes do. Also mention the number of the issue where discussion has taken
+   place. Preferably use our
+   [PR template](https://github.com/CodeForAfricaLabs/sensors.AFRICA-SeaSensors-Map/blob/master/.github/PULL_REQUEST_TEMPLATE.md).
+
+   Then sit back and wait. There will probably be discussion about the pull
+   request and, if any changes are needed, we would love to work with you to
+   get your pull request merged into sensors.AFRICA.
+
+## Ground Rules
+
+The goal is to maintain a diverse community that's pleasant for everyone.
+That's why we would greatly appreciate it if everyone contributing to and
+interacting with the community also followed this
+[Code of Conduct](https://www.contributor-covenant.org/version/1/4/code-of-conduct).
+The [Code of Conduct](https://www.contributor-covenant.org/version/1/4/code-of-conduct) covers our
+behavior as members of the community, in any forum, mailing list, wiki, website,
+Internet relay chat (IRC), public meeting or private correspondence.
+
+Please see [contribution-guide.org](http://www.contribution-guide.org) for details on what we expect from contributors. Thanks!
---
+++ src/components/Filter.tsx
@@ -0,0 +1,134 @@
+import React, { createRef } from 'react';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    zIndex: 1,
+    position: 'absolute',
+    width: '10rem',
+    lineHeight: '15px',
+    marginTop: '10rem',
+    marginLeft: '4rem',
+    marginBottom: '4rem',
+    boxShadow: '0 1px 2px rgba(0, 0, 0, 0.1)',
+    overflow: 'scroll',
+    backgroundColor: 'white'
+  },
+  header: {
+    display: 'inline-block',
+    height: '3rem',
+    width: '100%',
+    backgroundColor: '#4a345a',
+    color: 'white',
+    padding: ' 0.5rem'
+  },
+  filterGroup: {
+    marginLeft: '1.5rem',
+    marginRight: '1.5rem',
+    '& input[type="checkbox"]': {
+      appearance: 'radio',
+      marginBottom: '10px'
+    }
+  },
+  filterTitle: {
+    color: 'grey'
+  },
+  filterLabel: {
+    display: 'flex',
+    alignItems: 'baseline',
+    color: 'grey'
+  }
+}));
+
+export type FilterValue = [string, any];
+
+interface Props {
+  onFilter: (filter: Array<FilterValue>) => void;
+}
+
+export default function Filter({ onFilter }: Props) {
+  const classes = useStyles();
+  const filterGroupRef = createRef<HTMLDivElement>();
+
+  const handleFilter = () => {
+    if (filterGroupRef.current) {
+      const inputs = filterGroupRef.current.querySelectorAll(
+        'input[type=checkbox]'
+      );
+      const checked = Array.from(inputs).filter(
+        (element: any) => element.checked
+      ) as HTMLInputElement[];
+      onFilter(checked.map(e => [e.name, e.value]));
+    }
+  };
+
+  return (
+    <div className={classes.root}>
+      <div className={classes.header}>
+        <h2>Filters</h2>
+      </div>
+      <div
+        ref={filterGroupRef}
+        className={classes.filterGroup}
+        onClick={handleFilter}
+      >
+        <h4 className={classes.filterTitle}>Tide</h4>
+        <label className={classes.filterLabel}>
+          <input type="checkbox" name="Tide" value="Spring" />
+          Spring
+        </label>
+        <label className={classes.filterLabel}>
+          <input type="checkbox" name="Tide" value="Normal" />
+          Normal
+        </label>
+        <h4 className={classes.filterTitle}>Tidal state</h4>
+        <label className={classes.filterLabel}>
+          <input type="checkbox" name="Tidal State" value="Falling" />
+          Falling
+        </label>
+        <label className={classes.filterLabel}>
+          <input type="checkbox" name="Tidal State" value="High" />
+          High
+        </label>
+        <h4 className={classes.filterTitle}>Time of Day</h4>
+        <label className={classes.filterLabel}>
+          <input type="checkbox" name="Time of Day" value="AM" />
+          AM
+        </label>
+        <label className={classes.filterLabel}>
+          <input type="checkbox" name="Time of Day" value="PM" />
+          PM
+        </label>
+        <h4 className={classes.filterTitle}>Day</h4>
+        <label className={classes.filterLabel}>
+          <input type="checkbox" name="Day" value="Monday" />
+          Monday
+        </label>
+        <label className={classes.filterLabel}>
+          <input type="checkbox" name="Day" value="Tuesday" />
+          Tuesday
+        </label>
+        <label className={classes.filterLabel}>
+          <input type="checkbox" name="Day" value="Wednesday" />
+          Wednesday
+        </label>
+        <label className={classes.filterLabel}>
+          <input type="checkbox" name="Day" value="Tuesday" />
+          Thursday
+        </label>
+        <label className={classes.filterLabel}>
+          <input type="checkbox" name="Day" value="Friday" />
+          Friday
+        </label>
+        <label className={classes.filterLabel}>
+          <input type="checkbox" name="Day" value="Saturday" />
+          Saturday
+        </label>
+        <label className={classes.filterLabel}>
+          <input type="checkbox" name="Day" value="Sunday" />
+          Sunday
+        </label>
+      </div>
+    </div>
+  );
+}
